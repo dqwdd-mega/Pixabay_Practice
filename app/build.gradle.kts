161b49d78e71b421ded34379ef76a9b97141f031
dqwdd-mega <dqwdd@megastudy.net>
2025-11-06 14:23:40 +0900

    feat : TargetSDK 변경(36 -> 35)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.tving_task_2025_11_05"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.tving_task_2025_11_05"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
